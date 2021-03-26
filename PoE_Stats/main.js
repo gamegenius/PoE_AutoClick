86efb6d63af49c678fb27930a9ba86a03b6b6c7d
gamegenius <liuchar@gmail.com>
2021-03-27 04:58:15 +0800

    Update main.js

--- PoE_Stats/main.js
+++ PoE_Stats/main.js
@@ -1,4 +1,5 @@
 const https = require("https");
+const fs = require("fs");
 
 https.get('https://web.poe.garena.tw/api/trade/data/stats', (res) => {
     let rowdata = "";
@@ -7,11 +8,20 @@ https.get('https://web.poe.garena.tw/api/trade/data/stats', (res) => {
     });
     res.on('end', () => {
         let json_data = JSON.parse(rowdata);
-        json_data.result.forEach(Label => {
+        json_data.result.forEach((Label) => {
             //console.log(Label);
             if (Label.label == "隨機屬性") {
-                Label.entries.forEach(stats => {
-                    console.log(stats.text);
+                let data = "";
+                Label.entries.forEach((stats, index) => {
+                    if (index == (Label.entries.length - 1)) {
+                        data += stats.text + "\n";
+                        fs.writeFile("stats.txt", data, (err) => {
+                            if (err) throw err;
+                            console.log('The file has been saved!');
+                        });
+                    } else {
+                        data += stats.text + "\n";
+                    }
                 })
             }
         });
